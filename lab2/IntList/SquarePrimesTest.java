2068b5601c15ef4fb9b6568eb60b12758946c4d1
qiushao <qiushao418@sjtu.edu.cn>
2024-07-02 23:20:29 +0800

    Finish Lab 2.

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -11,10 +11,42 @@ public class SquarePrimesTest {
      * is broken. Write your own JUnit Test to try to uncover the bug!
      */
     @Test
-    public void testSquarePrimesSimple() {
+    public void testSquarePrimesSimple1() {
         IntList lst = IntList.of(14, 15, 16, 17, 18);
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void test2() {
+        IntList lst = IntList.of(3, 4, 4, 5);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9 -> 4 -> 4 -> 25", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void test3() {
+        IntList lst = IntList.of(4, 4, 4, 4);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 4 -> 4 -> 4", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void test4() {
+        IntList lst = IntList.of(3, 4, 3, 4);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9 -> 4 -> 9 -> 4", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void test5() {
+        IntList lst = IntList.of(4, 5);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 25", lst.toString());
         assertTrue(changed);
     }
 }
