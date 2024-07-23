6cc36bf7759c71bfecd09a0120d8b5f08bb51ea5
qiushao <qiushao418@sjtu.edu.cn>
2024-07-23 20:48:26 +0800

    finished Lab 6

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,17 +1,17 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author Qiushao
 */
-public class Dog { // TODO
+public class Dog implements Serializable{ //
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = join(CapersRepository.CAPERS_FOLDER, "dogs");
 
     /** Age of dog. */
     private int age;
@@ -39,8 +39,8 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        File dog = new File(".capers/dogs/" + name);
+        return readObject(dog, Dog.class);
     }
 
     /**
@@ -56,7 +56,13 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File dog = new File(".capers/dogs/" + this.name);
+        try {
+            dog.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        writeObject(dog, this);
     }
 
     @Override
